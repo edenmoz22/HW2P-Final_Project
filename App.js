0f56c7f73ee513f943c9cd3d53a79ff88d6bb9b7
Sitara Alayev <sitaraalayev@gmail.com>
2021-11-11 10:50:36 +0200

    added a dataset

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App()
+ {
+   const [outputText,setOutputText]=useState('test');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding:50,width:'80%',height:300,justifyContent:'space-between',alignItems:'center'}}>
+      <Text>HW2P-ברוכים הבאים ל</Text>
+      <Button title="צפייה בתיקיות" onPress={()=> setOutputText("כפתור 0 עובד")}/>
+      <Button title="העלאת תמונה לסריקה" onPress={()=> setOutputText("כפתור 2 עובד")}/>
+      <Button title="אימון בסיס נתונים" onPress={()=> setOutputText("כפתור 3 עובד")}/>
+      <Text>{outputText}</Text>
       <StatusBar style="auto" />
     </View>
   );
