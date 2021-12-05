57b7fd8431dd18ef4b03804a05ae475fcbc3f682
Sitara Alayev <sitaraalayev@gmail.com>
2021-12-05 19:06:24 +0200

    New component for input from user.

---
+++ components/Input.js
@@ -0,0 +1,14 @@
+/*import React from "react";
+import { TextInput } from "react-native";
+import Styles from '../Styles';
+
+const Input = props => {
+    return <TextInput style={Styles.inputText}/>;
+
+    
+};
+
+
+
+
+export default Input*/
