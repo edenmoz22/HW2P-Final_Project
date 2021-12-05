e0cd2b6563bd12b4138776aad7b6b0cf87c10bab
Sitara Alayev <sitaraalayev@gmail.com>
2021-12-05 19:09:50 +0200

    New component for selecting options.

---
+++ components/Select_Box.js
@@ -0,0 +1,15 @@
+/*import React from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
+import Styles from '../Styles';
+
+
+export const Selec_Box = (props) =>
+ {
+  return (
+      <TouchableOpacity onPress={props.onPress}>
+        <View style={{...Styles.SelectContainer, ...Styles.SelectContainer_buttons, ...props.style}}>{props.children} </View>
+      </TouchableOpacity>
+
+  );
+}
+export default Select_Box*/
