89e3a64fa33a72a154353aa23eca44ceb671e368
Sitara Alayev <sitaraalayev@gmail.com>
2021-12-07 11:15:19 +0200

    Change view of screen .

--- screens/Upload_image_scan.js
+++ screens/Upload_image_scan.js
@@ -1,18 +1,28 @@
-/*import React from "react";
+import React from "react";
 import { View,Text } from "react-native";
 import Styles from "../Styles";
-import Select_Box, { Selec_Box } from "../components/Select_Box";
+import  {Select_Box } from "../components/Select_Box";
+import {Main_button} from "../components/Main_button";
 
 
-const Upload_image_scan =() =>{
+export const Upload_image_scan =() =>{
     return(
-
-        <View style={
-           ?האם תרצה לעלות תמונה מהגלריה או לצלם תמונה
-
-            </View>
-
-        </Select_Box>        
+        <View style={Styles.container}>
+            <View style={Styles.SelectContainer}>
+             <Text style={Styles.textstyle} >
+                ?האם תרצה לעלות תמונה מהגלריה או לצלם במצלמה
+                </Text>
+                <Main_button style={Styles.SelectContainer_buttons}>
+                     מצלמה
+                </Main_button>
+                <Main_button style={Styles.SelectContainer_buttons}>
+                     גלריה
+                </Main_button>
+                </View>
+        </View>
+        
 
     );
-};*/
+};
+
+export default Upload_image_scan
